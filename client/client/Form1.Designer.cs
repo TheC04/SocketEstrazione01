b8ab64e020940db912a9cda47c92296c6a449401
Utente <Utente@DESKTOP-727B2FQ>
2022-10-05 16:59:40 +0200

    Aggiungere i file di progetto.

---
+++ client/client/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.Label();
+            this.messages = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(317, 203);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(52, 23);
+            this.send.TabIndex = 0;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(108, 203);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(128, 20);
+            this.name.TabIndex = 1;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(104, 138);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(265, 44);
+            this.title.TabIndex = 2;
+            this.title.Text = "Write your name and suraname,\r\nno spaces all lower";
+            // 
+            // messages
+            // 
+            this.messages.Location = new System.Drawing.Point(485, 59);
+            this.messages.Name = "messages";
+            this.messages.Size = new System.Drawing.Size(231, 280);
+            this.messages.TabIndex = 3;
+            this.messages.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 381);
+            this.Controls.Add(this.messages);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.send);
+            this.MaximumSize = new System.Drawing.Size(486, 420);
+            this.MinimumSize = new System.Drawing.Size(486, 420);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label messages;
+    }
+}
+
---
+++ client/client/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            string Sender = name.Text;
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+
+                Socket send = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    send.Connect(remoteEP);
+
+                    messages.Text = "C:\tSocket connected to " + send.RemoteEndPoint.ToString() + "\n";
+
+                    messages.Text = messages.Text + "C:\tI'm " + Sender + " can you give me a number?\n";
+                    byte[] msg = Encoding.ASCII.GetBytes("I'm " + Sender + " can you give me a number?<EOF>");
+
+                    int bytesSent = send.Send(msg);
+
+                    int bytesRec = send.Receive(bytes);
+                    if (bytes.ToString() == "You are not in the list. If you want to be added contact the developer")
+                    {
+                        messages.Text = messages.Text + ("S:\tYou are not in the list. If you want to be added contact the developer");
+                    }
+                    else
+                    {
+                        messages.Text = messages.Text + ("S:\tThe random number is " + Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                    }
+
+                    send.Shutdown(SocketShutdown.Both);
+                    send.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception en)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", en.ToString());
+                }
+
+            }
+            catch (Exception en)
+            {
+                Console.WriteLine(en.ToString());
+            }
+        }
+    }
+}
