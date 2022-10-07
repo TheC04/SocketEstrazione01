7bbfd53bf13ff90ae81f44b12e13df9feac97ecc
TheC04 <salvimarco04@gmail.com>
2022-10-07 13:53:38 +0200

    creazione interfaccia grafica

--- server/server/Form1.Designer.cs
+++ server/server/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace server
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.OnOff = new System.Windows.Forms.Button();
+            this.state = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OnOff
+            // 
+            this.OnOff.Location = new System.Drawing.Point(423, 154);
+            this.OnOff.Name = "OnOff";
+            this.OnOff.Size = new System.Drawing.Size(75, 23);
+            this.OnOff.TabIndex = 0;
+            this.OnOff.Text = "Accendi";
+            this.OnOff.UseVisualStyleBackColor = true;
+            this.OnOff.Click += new System.EventHandler(this.OnOff_Click);
+            // 
+            // state
+            // 
+            this.state.AutoSize = true;
+            this.state.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.state.Location = new System.Drawing.Point(90, 144);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(305, 31);
+            this.state.TabIndex = 1;
+            this.state.Text = "Stato del server: Spento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(683, 351);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.OnOff);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button OnOff;
+        public static System.Windows.Forms.Label state;
     }
 }
 
--- server/server/Form1.cs
+++ server/server/Form1.cs
@@ -12,9 +12,25 @@ namespace server
 {
     public partial class Form1 : Form
     {
+        string status = "Stato del server: ";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OnOff_Click(object sender, EventArgs e)
+        {
+            if (OnOff.Text == "Accendi")
+            {
+                OnOff.Text = "Spegni";
+                //Form1.state.Text = status + "Attivo";
+            }
+            else
+            {
+                OnOff.Text = "Accendi";
+               // Form1.state.Text = status + "Spento";
+            }
         }
     }
 }
